31ff2fff3d7f08eeabaf9029bd1730dd86bffbd3
Jesús Higueras <jesus@dabbleam.com>
2022-09-19 22:55:53 +0200

    ui/gtk: make sidebar panels useful and prettier

--- ui/gtk/components/sidebar/panel.lua
+++ ui/gtk/components/sidebar/panel.lua
@@ -1,20 +1,35 @@
 local lgi = require "lgi"
 
 local Gtk = lgi.Gtk
+local Gdk = lgi.Gdk
 
 local state = require "core.state"
 
 local panel = {}
 panel.__index = panel
 
+local _initialized = false
+
 function panel.new( title )
+    if not _initialized then
+        _initialized = true
+        local cssProvider = Gtk.CssProvider()
+        cssProvider:load_from_data( [[
+            .sidebar-header {
+                box-shadow: inset 0 -4px 4px -6px rgba(0, 0, 0, 0.5);
+            }
+        ]] )
+
+        Gtk.StyleContext.add_provider_for_screen( Gdk.Screen.get_default(), cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION )
+    end
+
     local self = {
         element = panel
     }
 
     local panelInstance = Gtk.Box {
         orientation = Gtk.Orientation.VERTICAL,
-        spacing = 10,
+        spacing = 0,
         hexpand = true,
         vexpand = true,
     }
@@ -23,11 +38,9 @@ function panel.new( title )
         orientation = Gtk.Orientation.HORIZONTAL,
         hexpand = true,
         vexpand = false,
-        margin_top = 10,
-        margin_bottom = 10,
-        margin_left = 10,
-        margin_right = 10
     }
+
+    panelHeader:get_style_context():add_class( "sidebar-header" )
 
     local panelTitle = Gtk.Label {
         label = title
@@ -40,20 +53,36 @@ function panel.new( title )
         yalign = 0.5,
         hexpand = true,
         vexpand = false,
-        child = panelTitle
+        child = panelTitle,
+        margin_top = 8,
+        margin_bottom = 7,
+        margin_left = 10,
+        margin_right = 10
     }
 
     panelHeader:pack_start( panelTitleAlignment, true, true, 0 )
 
+    local panelBody = Gtk.Box {
+        orientation = Gtk.Orientation.VERTICAL,
+        hexpand = true,
+        vexpand = true
+    }
+
     panelInstance:pack_start( panelHeader, false, false, 0 )
+    panelInstance:pack_start( panelBody, true, true, 0 )
 
     local self = {
-        element = panelInstance
+        element = panelInstance,
+        body = panelBody
     }
 
     setmetatable( self, panel )
 
     return self
+end
+
+function panel:add( element )
+    self.body:pack_start( element, true, true, 0 )
 end
 
 return panel
